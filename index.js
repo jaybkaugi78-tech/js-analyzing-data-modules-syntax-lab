62e744a67da8bc6d90008c0e1d3ff86882691142
Jeremy Gitonga <jaybkaugi78@gmail.com>
2026-04-15 21:40:14 +0300

    final work

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const datejs = require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (const array of args) {
+    combinedObject.users = [...combinedObject.users, ...array];
+  }
+
+  combinedObject.merge_date = Date.today().toString( `M/d/yyyy` );
+
+  return combinedObject;
+}
 
 
 
